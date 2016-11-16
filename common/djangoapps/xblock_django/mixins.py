2e1567cbd37ac0b8524f962f8cd4eff557630d8f
Ivan Kovic <kovic.ivan@gmail.com>
2016-11-16 12:37:35 +0100

    Only delete old file if saving the new file succeeded

--- common/djangoapps/xblock_django/mixins.py
+++ common/djangoapps/xblock_django/mixins.py
@@ -14,21 +14,18 @@ class FileUploadMixin(object):
 
     def upload_to_s3(self, file_type, file, xblock_id, file_url_to_delete=None):
         bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-
-        # delete the previously uploaded file if it exists on S3
-        if file_url_to_delete and bucket_name in file_url_to_delete:
-            name_to_delete = file_url_to_delete.split(bucket_name)[1]
-            if default_storage.exists(name_to_delete):
-                default_storage.delete(name_to_delete)
-
         file_uuid = str(uuid.uuid1())
-
         content = ContentFile(file.read())
-
         hashed_xblock_id = str(hash(xblock_id))
 
         relative_path = default_storage.save(
             self._file_types[file_type] + hashed_xblock_id + '/' + file_uuid + '_' + file.name,
             content)
+
+        # delete the previously uploaded file if it exists on S3
+        if file_url_to_delete and bucket_name in file_url_to_delete:
+            name_to_delete = file_url_to_delete.split(bucket_name, 1)[1]
+            if default_storage.exists(name_to_delete):
+                default_storage.delete(name_to_delete)
 
         return settings.AWS_S3_BASE_URL + bucket_name + relative_path
